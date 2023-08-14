fe4e6a11f0ab1640c445e6d0bcccac541c1038c0
David <davidsejer@gmail.com>
2023-08-14 14:29:06 +0200

    Added login to loginpage

---
+++ Xecurity App/View/LoginPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System;
+using System.Text.Json;
+using System.Diagnostics;
+using Xecurity_App.Model;
+
+namespace Xecurity_App;
+
+public partial class LoginPage : ContentPage
+{
+    HttpClient _client;
+    JsonSerializerOptions _serializerOptions;
+
+    public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        _client = new HttpClient();
+        var username = UserEntry.Text;
+        var password = PasswordEntry.Text;
+
+        var soundData = new PostUserLogin() { username = username, password = password };
+
+        var json = JsonSerializer.Serialize(soundData);
+        var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var url = "http://192.168.1.122:7032/api/Auth/login";
+
+        var response = await _client.PostAsync(url, data);
+
+        string result = response.Content.ReadAsStringAsync().Result;
+
+        if(response.IsSuccessStatusCode)
+        {
+        }
+        await Application.Current.MainPage.DisplayAlert("Sent", result, "ok");
+
+    } 
+}
+
